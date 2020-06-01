8d0cd02ffc7d9b66dda30500a623107a54eee5fe
Kirillosman <61022625+Kirillosman@users.noreply.github.com>
2020-06-01 17:15:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-
 const calcBtnsEl = document.getElementById('calc-buttons');
 let activeNum  = document.getElementById('num1');
 let activeSign  = document.getElementById('sign');
+const arr = ['+', '-', '/', '*'];
 
 calcBtnsEl.addEventListener('click', (ev) => {
   if(ev.target.nodeName === 'BUTTON' && !ev.target.dataset.sign) {
@@ -9,65 +9,62 @@ calcBtnsEl.addEventListener('click', (ev) => {
     activeNum.innerText += attrVal;
   }
 
-  if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '+') {
+  if(ev.target.nodeName === 'BUTTON' && arr.indexOf(ev.target.dataset.sign) !== -1) {
     const attrSign = ev.target.getAttribute('data-sign');
     activeSign.innerText = attrSign;
     activeNum = document.getElementById('num2');
   }
 
-  if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '=') {
+  if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '=' && activeSign.innerText === '+') {
     let num1Val = +document.getElementById('num1').innerText;
     let num2Val = +document.getElementById('num2').innerText;
+    document.getElementById('num1').innerText = "";
+    document.getElementById('num2').innerText = "";
+    activeSign.innerText = "";
 
     const result = num1Val + num2Val; 
     document.querySelector('#result span').innerText = result;
-  }
-
-  if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '-') {
-    const attrSign = ev.target.getAttribute('data-sign');
-    activeSign.innerText = attrSign;
-    activeNum = document.getElementById('num2');
+    
   }
 
   if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '=' && activeSign.innerText === '-') {
     let num1Val = document.getElementById('num1').innerText;
     let num2Val = document.getElementById('num2').innerText;
+    document.getElementById('num1').innerText = "";
+    document.getElementById('num2').innerText = "";
+    activeSign.innerText = "";
 
     const result = num1Val - num2Val; 
     document.querySelector('#result span').innerText = result;
   }
 
-  if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '/') {
-    const attrSign = ev.target.getAttribute('data-sign');
-    activeSign.innerText = attrSign;
-    activeNum = document.getElementById('num2');
-  }
-
   if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '=' && activeSign.innerText === '/') {
     let num1Val = document.getElementById('num1').innerText;
     let num2Val = document.getElementById('num2').innerText;
+    document.getElementById('num1').innerText = "";
+    document.getElementById('num2').innerText = "";
+    activeSign.innerText = "";
 
     const result = num1Val / num2Val; 
     document.querySelector('#result span').innerText = result;
   }
 
-  if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '*') {
-    const attrSign = ev.target.getAttribute('data-sign');
-    activeSign.innerText = attrSign;
-    activeNum = document.getElementById('num2');
-  }
-
   if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === '=' && activeSign.innerText === '*') {
     let num1Val = document.getElementById('num1').innerText;
     let num2Val = document.getElementById('num2').innerText;
+    document.getElementById('num1').innerText = "";
+    document.getElementById('num2').innerText = "";
+    activeSign.innerText = "";
 
     const result = num1Val * num2Val; 
     document.querySelector('#result span').innerText = result;
   }
+
+  if(ev.target.nodeName === 'BUTTON' && ev.target.dataset.sign === 'c') {
+    document.getElementById('num1').innerText = "";
+    document.getElementById('num2').innerText = "";
+    activeSign.innerText = "";
+    document.querySelector('#result span').innerText = "";
+    activeNum = document.getElementById('num1');
+  }
 })
-
-
-
-
-
-
